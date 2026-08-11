d6a2535db48b3a2f2b596578a95117c2a5783a09
mihaicismaru-bit <mihai.cismaru@gmail.com>
2026-08-11 20:33:07 +0300

    Add MIPE news bootstrap feed

---
+++ partener-eu/web/mipe-news.js
@@ -0,0 +1,3 @@
+window.PARTENER_DATA=window.PARTENER_DATA||{};
+window.PARTENER_DATA.mipeIngestion={status:"BOOTSTRAP",asOf:null,source:"MIPE official web properties",roots:[],itemCount:0};
+window.PARTENER_DATA.mipeNews=[];
