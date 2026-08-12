9f6c14feba8a205a329d08eb8d54fcbf379860bf
mihaicismaru-bit <mihai.cismaru@gmail.com>
2026-08-12 07:38:00 +0300

    Initialize PEO calendar data asset

---
+++ partener-eu/web/peo-calendar.js
@@ -0,0 +1,2 @@
+window.PARTENER_DATA=window.PARTENER_DATA||{};
+window.PARTENER_DATA.peoCalendar={"status":"AWAITING_FIRST_INGESTION","programme":"PEO","title":"Calendar estimativ consolidat al lansărilor de apeluri de proiecte","directMipeVerified":false,"itemCount":0,"changeCount":0,"items":[],"changes":[]};
